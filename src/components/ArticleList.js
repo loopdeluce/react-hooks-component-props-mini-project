337ca2fc84781395c58fb821f6bd8807e28aa573
Lucy Bullen <102437824+loopdeluce@users.noreply.github.com>
2022-06-07 09:45:11 -0600

    commit tree f183d792846e4b475051407093037dd9af830cb3

---
+++ src/components/ArticleList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Article from './Article';
+
+function ArticleList({posts}){
+  const postElements = posts.map(post => (
+    <Article 
+      key={post.id} 
+      title={post.title} 
+      date={post.title} 
+      preview={post.preview}
+    />
+  ));
+  return ( 
+    <main>
+      {postElements}
+    </main>
+  );
+};
+
+export default ArticleList;
